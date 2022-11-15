130431933f5a3311ff245497a3192d95d489ba4b
Haelyne <114979106+Haelyne@users.noreply.github.com>
2022-11-14 16:43:02 -0800

    Add files via upload

---
+++ parts/navbar.php
@@ -0,0 +1,17 @@
+<header class="navbar">
+	<div class="cointainer display-flex">
+		<div class="flex-none">
+			<h1>Fruit Store</h1>
+		</div>
+
+	<dix class="flex-stretch"></dix>
+	<div class="flex-none nav nav-flex">
+		<ul>
+			<li><a href="index.php">Home</a></li>
+			<li><a href="product_list.php">Product List</a></li>
+			<li><a href="about.php">About Us</a></li>
+			<li><a href="cart.php">Cart</a></li>
+		</ul>
+	</div>
+	</div>
+</header>
